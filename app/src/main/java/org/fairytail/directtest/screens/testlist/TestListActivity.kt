9236e13cc73de2a48ac4cef16ccccb87ad53ca68
Valentine <valentine.trifonov@gmail.com>
2017-05-13 13:57:58 +0200

    test list activity

---
+++ app/src/main/java/org/fairytail/directtest/screens/testlist/TestListActivity.kt
@@ -0,0 +1,45 @@
+package org.fairytail.directtest.screens.testlist
+
+import android.databinding.ObservableArrayList
+import android.os.Bundle
+import android.view.View
+import com.github.nitrico.lastadapter.LastAdapter
+import com.github.nitrico.lastadapter.Type
+import kotlinx.android.synthetic.main.activity_test_list.*
+import org.fairytail.directtest.BR
+import org.fairytail.directtest.R
+import org.fairytail.directtest.base.BaseBoundActivity
+import org.fairytail.directtest.databinding.ActivityTestListBinding
+import org.fairytail.directtest.databinding.ItemTestBinding
+import org.fairytail.directtest.models.Test
+import org.jetbrains.anko.toast
+
+/**
+ * Created by Valentine on 5/13/2017.
+ */
+class TestListActivityViewModel {
+    fun onFabCreateTestClick(v: View?) {
+        v?.context?.toast("fab clicked")
+    }
+}
+
+class TestListActivity : BaseBoundActivity<ActivityTestListBinding>(R.layout.activity_test_list) {
+    val testList = ObservableArrayList<Test>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Test.getAll()
+                .subscribe {
+                    testList.clear()
+                    testList.addAll(it)
+                }
+
+        LastAdapter(testList, BR.item)
+                .type { _, _ ->
+                    Type<ItemTestBinding>(R.layout.item_test)
+                            .onClick { toast("Clicked #${it.adapterPosition}: ${it.binding.item}") }
+                }
+                .into(list_test)
+    }
+}
