c06f6f458bd9de2a16c58a31a11d8c7bc58d612a
Valentine <valentine.trifonov@gmail.com>
2017-05-13 12:33:56 +0300

    main, base activities

---
+++ app/src/main/java/org/fairytail/directtest/MainActivity.kt
@@ -0,0 +1,10 @@
+package org.fairytail.directtest
+
+import org.fairytail.directtest.base.BaseActivity
+
+/**
+ * Created by Valentine on 5/13/2017.
+ */
+class MainActivity : BaseActivity() {
+
+}
---
+++ app/src/main/java/org/fairytail/directtest/base/BaseActivity.kt
@@ -0,0 +1,10 @@
+package org.fairytail.directtest.base
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Valentine on 5/13/2017.
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+}
