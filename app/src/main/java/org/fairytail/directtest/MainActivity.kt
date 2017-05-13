9c8c5c0a158115d867bb824e65442b455cb6ae4b
Valentine <valentine.trifonov@gmail.com>
2017-05-13 12:46:40 +0300

    main, base activities

--- app/src/main/java/org/fairytail/directtest/MainActivity.kt
+++ app/src/main/java/org/fairytail/directtest/MainActivity.kt
@@ -1,10 +1,11 @@
 package org.fairytail.directtest
 
-import org.fairytail.directtest.base.BaseActivity
+import org.fairytail.directtest.base.BaseBoundActivity
+import org.fairytail.directtest.databinding.ActivityMainBinding
 
 /**
  * Created by Valentine on 5/13/2017.
  */
-class MainActivity : BaseActivity() {
+class MainActivity : BaseBoundActivity<ActivityMainBinding>(R.layout.activity_main) {
 
 }
--- app/src/main/java/org/fairytail/directtest/base/BaseActivity.kt
+++ app/src/main/java/org/fairytail/directtest/base/BaseActivity.kt
@@ -1,10 +1,48 @@
 package org.fairytail.directtest.base
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+import io.realm.Realm
 
 /**
  * Created by Valentine on 5/13/2017.
  */
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : RxAppCompatActivity() {
+    protected open val layoutId: Int?
+        get() = null
 
+    protected val realm: Realm by lazy { Realm.getDefaultInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (layoutId != null) setContentView(layoutId!!)
+    }
+
+    override fun onDestroy() {
+        realm.close()
+        super.onDestroy()
+    }
+}
+
+abstract class BaseBoundActivity<out T : ViewDataBinding>(
+        protected val layoutId: Int,
+        protected val disableTransitions: Boolean = true
+) : RxAppCompatActivity() {
+    private lateinit var innerBinding: T
+    protected val binding: T by lazy { innerBinding }
+
+    protected val realm: Realm by lazy { Realm.getDefaultInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (disableTransitions) overridePendingTransition(0, 0)
+        innerBinding = DataBindingUtil.setContentView(this, layoutId)
+    }
+
+    override fun onDestroy() {
+        realm.close()
+        super.onDestroy()
+    }
 }
