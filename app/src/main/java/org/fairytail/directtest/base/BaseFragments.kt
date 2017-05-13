2f682da6e43556d36df9ce86de571caaed59ac31
Valentine <valentine.trifonov@gmail.com>
2017-05-13 12:57:33 +0300

    base activities & fragments

---
+++ app/src/main/java/org/fairytail/directtest/base/BaseFragments.kt
@@ -0,0 +1,64 @@
+package org.fairytail.directtest.base
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.trello.rxlifecycle2.components.support.RxFragment
+import io.realm.Realm
+
+/**
+ * Created by Valentine on 5/13/2017.
+ */
+abstract class BaseFragment(
+        protected val layoutId: Int
+) : RxFragment() {
+    private lateinit var innerRealm: Realm
+    protected val realm
+        get() = innerRealm
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(layoutId, container, false)
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        innerRealm = Realm.getDefaultInstance()
+    }
+
+    override fun onDestroy() {
+        innerRealm.close()
+        super.onDestroy()
+    }
+}
+
+abstract class BaseBoundFragment<out T : ViewDataBinding>(
+        protected val layoutId: Int
+) : RxFragment() {
+    private lateinit var innerBinding: T
+    protected val binding
+        get() = innerBinding
+
+    private lateinit var innerRealm: Realm
+    val realm
+        get() = innerRealm
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(layoutId, container, false)
+        innerBinding = DataBindingUtil.bind(view)
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        innerRealm = Realm.getDefaultInstance()
+    }
+
+    override fun onDestroy() {
+        innerRealm.close()
+        super.onDestroy()
+    }
+}
