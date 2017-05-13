d153b3eaba869f4913ee47d99b83dd4308d678a0
Valentine <valentine.trifonov@gmail.com>
2017-05-13 14:15:30 +0200

    await activity wip

--- app/src/main/java/org/fairytail/directtest/MainActivity.kt
+++ app/src/main/java/org/fairytail/directtest/MainActivity.kt
@@ -1,11 +1,26 @@
 package org.fairytail.directtest
 
+import android.view.View
 import org.fairytail.directtest.base.BaseBoundActivity
 import org.fairytail.directtest.databinding.ActivityMainBinding
+import org.fairytail.directtest.screens.student.AwaitActivity
+import org.fairytail.directtest.screens.teacher.testlist.TestListActivity
+import org.jetbrains.anko.startActivity
 
 /**
  * Created by Valentine on 5/13/2017.
  */
+class MainActivityViewModel {
+    fun onTeacherClick(v: View?) {
+        v?.context?.startActivity<TestListActivity>()
+    }
+
+    fun onStudentClick(v: View?) {
+        //TODO: go to wait screen
+        v?.context?.startActivity<AwaitActivity>()
+    }
+}
+
 class MainActivity : BaseBoundActivity<ActivityMainBinding>(R.layout.activity_main) {
 
 }
